23c468a85e86a3f70ffa260dc1c211583dfe0cbe
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-06 11:40:13 +0200

    Test 1 passé

---
+++ P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
@@ -0,0 +1,20 @@
+﻿
+TestComptageMots(1, "", 0);
+
+int CompteMots(string phrase)
+{
+    return 0;
+}
+
+void TestComptageMots(int testNumber, string phrase, int expectedOutput)
+{
+    int output = CompteMots(phrase);
+    if (output == expectedOutput)
+    {
+        Console.WriteLine($"Le test n°{testNumber} a réussi: \"{phrase}\" a {output} mots");
+    }
+    else
+    {
+        Console.WriteLine($"Le test n°{testNumber} a échoué: \"{phrase}\" a {output} mots ({expectedOutput} attendu)");
+    }
+}
