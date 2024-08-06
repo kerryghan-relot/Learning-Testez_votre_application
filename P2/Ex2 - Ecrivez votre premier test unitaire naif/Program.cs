ffb60ec38f26357a2a86c71fcd3efd4bdef9f8be
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-06 11:52:01 +0200

    Test 4 passé

--- P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
+++ P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
@@ -1,12 +1,19 @@
 ﻿
 TestComptageMots(1, "", 0);
 TestComptageMots(2, "mot", 1);
-TestComptageMots(3, "Deux mots", 2); 
+TestComptageMots(3, "Deux mots", 2);
+TestComptageMots(4, "Des Mots  avec des  espaces en trop  ", 7);
 
 int CompteMots(string phrase)
 {
     if (phrase.Length == 0) return 0;
-    return phrase.Count(chr => chr == ' ') + 1;
+    var mots = phrase.Split(' ');
+    int countWord = 0;
+    foreach (var mot in mots)
+    {
+        if (mot.Length > 0) countWord++;
+    }
+    return countWord;
 }
 
 void TestComptageMots(int testNumber, string phrase, int expectedOutput)
