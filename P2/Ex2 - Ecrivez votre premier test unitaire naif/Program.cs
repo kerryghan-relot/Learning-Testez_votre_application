1531d82213046d0ebf1b6ae2005224b489a7f079
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-06 13:47:22 +0200

    Test 10 fini !

--- P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
+++ P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
@@ -10,11 +10,12 @@ TestComptageMots(6, "Une phrase avec des accents où çà", 7);
 TestComptageMots(7, "Plusieurs phrases. Avec des points, et des virgules.", 8);
 TestComptageMots(8, "Une phrase ou j'ai mis une apostrophe", 8);
 TestComptageMots(9, " Plein de phrases;   avec une combinaison de ce que l'on a vu précédement.  ", 14);
+TestComptageMots(10, " Encore plus,compliqué; car il y  a des espaces en trop et   manquant! Mais aussi des accents à gogo.L'apostrophe c'est génial! Ãh Bon ? ", 26);
 
 int CompteMots(string? phrase)
 {
     if (phrase == null) return -1;
-    char[] delimiter = [ ',', ' ', ';', '\'' ];
+    char[] delimiter = [ ' ', ',', ';', '\'', '.', '!', '?' ];
     return phrase.Split(delimiter, StringSplitOptions.RemoveEmptyEntries).Length;
 }
 
