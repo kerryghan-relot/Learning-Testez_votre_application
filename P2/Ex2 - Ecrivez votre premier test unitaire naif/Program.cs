81b3b6fde557108076469f3a4646ff415e773480
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-06 11:48:00 +0200

    Fonction pour Test 3 remaniée (fonction CompteMots simplifié)

--- P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
+++ P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
@@ -6,12 +6,7 @@ TestComptageMots(3, "Deux mots", 2);
 int CompteMots(string phrase)
 {
     if (phrase.Length == 0) return 0;
-    int countSpace = 0;
-    foreach (char c in phrase)
-    {
-        if (c == ' ') countSpace++;
-    }
-    return countSpace + 1;
+    return phrase.Count(chr => chr == ' ') + 1;
 }
 
 void TestComptageMots(int testNumber, string phrase, int expectedOutput)
