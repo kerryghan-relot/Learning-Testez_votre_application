4f55d83081264c4d8cfb271ff2f72250689fcf3f
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-06 12:29:54 +0200

    Test 9 passé (précédent commit était test8)

--- P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
+++ P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using static System.Net.Mime.MediaTypeNames;
+
 TestComptageMots(1, "", 0);
 TestComptageMots(2, "mot", 1);
 TestComptageMots(3, "Deux mots", 2);
@@ -7,11 +9,13 @@ TestComptageMots(5, null, -1);
 TestComptageMots(6, "Une phrase avec des accents où çà", 7);
 TestComptageMots(7, "Plusieurs phrases. Avec des points, et des virgules.", 8);
 TestComptageMots(8, "Une phrase ou j'ai mis une apostrophe", 8);
+TestComptageMots(9, " Plein de phrases;   avec une combinaison de ce que l'on a vu précédement.  ", 14);
 
 int CompteMots(string? phrase)
 {
     if (phrase == null) return -1;
-    return phrase.Split(" ").Sum(mot => mot.Split("'").Count(m => m.Length > 0));
+    char[] delimiter = [ ',', ' ', ';', '\'' ];
+    return phrase.Split(delimiter, StringSplitOptions.RemoveEmptyEntries).Length;
 }
 
 void TestComptageMots(int testNumber, string? phrase, int expectedOutput)
