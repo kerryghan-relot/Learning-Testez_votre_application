89373b2edaa5e151f59694b4653efa8c6853fe99
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-06 12:08:27 +0200

    Test 9 passé

--- P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
+++ P2/Ex2 - Ecrivez votre premier test unitaire naif/Program.cs
@@ -6,11 +6,12 @@ TestComptageMots(4, "Des Mots  avec des  espaces en trop  ", 7);
 TestComptageMots(5, null, -1);
 TestComptageMots(6, "Une phrase avec des accents où çà", 7);
 TestComptageMots(7, "Plusieurs phrases. Avec des points, et des virgules.", 8);
+TestComptageMots(8, "Une phrase ou j'ai mis une apostrophe", 8);
 
 int CompteMots(string? phrase)
 {
     if (phrase == null) return -1;
-    return phrase.Split(' ').Count(mot => mot.Length > 0);
+    return phrase.Split(" ").Sum(mot => mot.Split("'").Count(m => m.Length > 0));
 }
 
 void TestComptageMots(int testNumber, string? phrase, int expectedOutput)
